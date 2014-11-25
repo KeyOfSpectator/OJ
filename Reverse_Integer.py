28c5a6e7322257cd316e900e053854df3b978234
KeyOfSpectator <413070456@qq.com>
2014-11-25 10:42:42 +0800

    fix Reverse_integer

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -2,27 +2,26 @@ class Solution:
     # @return an integer
     def reverse(self, x):
 
-    	flag = True
-    	if x<0:
-    		x = -x
-    		flag = False
+        flag = True
+        if x<0:
+            x = -x
+            flag = False
 
         num = 0
-        while x>10:
-        	num = num*10 + x%10
-        	x = x%10
+        while x>=10:
+            num = num*10 + x%10
+            x = x/10
             #print x
         if x>0:
-        	num = num*10 + x
+            num = num*10 + x
 
         if flag == False:
-         	num = -num
+             num = -num
 
         return num
 
 
 test = Solution()
-print test.reverse(1234)
-
+print test.reverse(10)
 
 
